de48168ca21f7316633378a6dcccd18f04b73735
Andrea Somma <120776791+sommaa@users.noreply.github.com>
2023-04-29 20:49:56 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Andrea",
+          to_name: "",
           from_email: form.email,
-          to_email: "sommandreaa@gmail.com",
+          to_email: "",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
